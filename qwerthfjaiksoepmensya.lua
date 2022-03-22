0c491a1f97e9280a5ea00932f815e110094f5b27
saopidjaopsdjp2wa <98464859+saopidjaopsdjp2wa@users.noreply.github.com>
2022-03-22 23:17:52 +0100

    Update qwerthfjaiksoepmensya.lua

--- qwerthfjaiksoepmensya.lua
+++ qwerthfjaiksoepmensya.lua
@@ -31,9 +31,7 @@ local DaHoodSettings = {
     Prediction = 0.178
 }
 
-local Tab1 = Window:CreateTab("Aiming")
-local Tab2 = Window:CreateTab("Visuals")
-local Tab3 = Window:CreateTab("Avatar")
+local Tab1 = Window:CreateTab("Main")
 local Tab4 = Window:CreateTab("Teleports")
 local Tab5 = Window:CreateTab("Misc")
 local Tab6 = Window:CreateTab("Credits")
@@ -41,8 +39,9 @@ local Tab7 = Window:CreateTab("Settings")
 
 local Section1 = Tab1:CreateSection("Silentaim Configuration")
 local Section2 = Tab1:CreateSection("Fov Configuration")
-local Section5 = Tab2:CreateSection("Esp Configuration")
-local Section6 = Tab3:CreateSection("Avatar Modifications")
+local Section5 = Tab1:CreateSection("Esp Configuration")
+local Section6 = Tab1:CreateSection("Avatar Modifications")
+local Section13 = Tab1:CreateSection("Faces")
 local Section7 = Tab4:CreateSection("Guns")
 local Section8 = Tab4:CreateSection("Places")
 local Section9 = Tab5:CreateSection("Misc")
@@ -52,14 +51,11 @@ local Section12 = Tab6:CreateSection("Partners")
 local Section3 = Tab7:CreateSection("Menu")
 local Section4 = Tab7:CreateSection("Background")
 
-local CreditsLabel = Section11:CreateLabel("Brought to you by hoodsense team.")
-CreditsLabel:UpdateText("Made with love by hoodsense team!")
-
-local PartnersLabel = Section12:CreateLabel("Brought to you by hoodsense team.")
-PartnersLabel:UpdateText("Partners supporting us!")
+local PartnersLabel = Section12:CreateLabel("Supporting with creating project!")
+PartnersLabel:UpdateText("Supporting with creating project!")
 
 ------------------------
------//AIMINGPAGE\\-----
+-----//MAIN  PAGE\\-----
 local SilentAimToggle = Section1:CreateToggle("Silent Aim", nil, function(bool)
     DaHoodSettings.SilentAim = bool
 end)
@@ -85,7 +81,7 @@ local HitAirShootsToggle = Section1:CreateToggle("Hit Airshots", nil, function(b
 end)
 HitAirShootsToggle:AddToolTip("Hits Airshots.")
 
-    FovCircleSize = Section2:CreateSlider("Fov Circle Size", 0,400,nil,false, function(value)
+local FovCircleSize = Section2:CreateSlider("Fov Circle Size", 0,400,nil,false, function(value)
     Aiming.FOV = value
 end)
 FovCircleSize:AddToolTip("Customize Fov Circle Size.")
@@ -94,6 +90,41 @@ local HitChanceSlider = Section2:CreateSlider("Hitchance", 0,400,nil,false, func
     DaHoodSettings.Prediction = tonumer("0." .. value)
 end)
 HitChanceSlider:AddToolTip("Customize hitchance.")
+
+local animationgamepass = Section6:CreateButton("Animation Gamepass", function()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/eksotopro/holders/main/animation_gamepass.lua'))()
+end)
+animationgamepass:AddToolTip("Loads Animation Gamepass \(Press P to close.\)")
+
+local meanie = Section13:CreateButton("Meanie", function()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/eksotopro/holders/main/faces/508490451.lua'))()
+end)
+meanie:AddToolTip("Changes your face to Meanie.")
+
+local sshf = Section13:CreateButton("Super Super Happy Face", function()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/eksotopro/holders/main/faces/494290547.lua'))()
+end)
+sshf:AddToolTip("Changes your face to Super Super Happy Face.")
+
+local silverpunk = Section13:CreateButton("Silver Punk Face", function()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/eksotopro/holders/main/faces/387256104.lua'))()
+end)
+silverpunk:AddToolTip("Changes your face to Silver Punk Face.")
+
+local yum = Section13:CreateButton("Yum", function()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/eksotopro/holders/main/faces/26018945.lua'))()
+end)
+yum:AddToolTip("Changes your face to Yum.")
+
+local playful = Section13:CreateButton("Playful Vampire", function()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/eksotopro/holders/main/faces/2409281591.lua'))()
+end)
+playful:AddToolTip("Changes your face to Playful Vampire.")
+
+local beastmode = Section13:CreateButton("Beast Mode", function()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/eksotopro/holders/main/faces/127959433.lua'))()
+end)
+beastmode:AddToolTip("Changes your face to Beast Mode.")
 ------------------------
 -----// ESP PAGE \\-----
 local espToggle = Section5:CreateToggle("Enable ESP", nil, function(bool)
@@ -120,7 +151,7 @@ tracersToggle:AddToolTip("Enables Boxes.")
 local animbutton = Section6:CreateButton("Animations", function(bool)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/eksotopro/holders/main/anims.lua"))()
 end)
-animbutton:AddToolTip("Avatar Modification. [NOT FE]")
+animbutton:AddToolTip("Changes your animations.")
 
 local headlbutton = Section6:CreateButton("Headless", function(bool)
 game.Players.LocalPlayer.Character.Head.Transparency = 1
@@ -130,7 +161,7 @@ v:Destroy()
 end
 end
 end)
-headlbutton:AddToolTip("Avatar Modification. [NOT FE]")
+headlbutton:AddToolTip("Gives you headless. [NOT FE]")
 
 local korbloxbutton = Section6:CreateButton("Korblox", function(bool)
     local ply = game.Players.LocalPlayer
@@ -144,17 +175,20 @@ local korbloxbutton = Section6:CreateButton("Korblox", function(bool)
 end)
 korbloxbutton:AddToolTip("Avatar Modification. [NOT FE]")
 
-local korbloxbuttonleft = Section6:CreateButton("Korblox Left", function(bool)
-    local ply = game.Players.LocalPlayer
-    local chr = ply.Character
-    chr.LeftLowerLeg.MeshId = "902942093"
-    chr.LeftLowerLeg.Transparency = "1"
-    chr.LeftUpperLeg.MeshId = "902942096"
-    chr.LeftUpperLeg.TextureID = "http://roblox.com/asset/?id=902843398"
-    chr.LeftFoot.MeshId = "902942089"
-    chr.LeftFoot.Transparency = "1"
+local animatedbeastmode = Section6:CreateButton("Animated Beast Mode", function()
+	while true do
+local player = game.Players.LocalPlayer.Character
+if player:findFirstChild("Humanoid") then
+    player.Head.face.Texture = "https://www.roblox.com/asset/?id=209712379"
+    end
+wait(1)
+if player:findFirstChild("Humanoid") then
+    player.Head.face.Texture = "https://www.roblox.com/asset/?id=127959433"
+    end
+wait(1)
+end
 end)
-korbloxbuttonleft:AddToolTip("Avatar Modification. [NOT FE]")
+animatedbeastmode:AddToolTip("Makes your face animated.")
 ------------------------
 -----// TP  PAGE \\-----
 local revolver = Section7:CreateButton("Revolver", function(bool)
@@ -197,14 +231,16 @@ local drumgun = Section7:CreateButton("Drum Gun", function(bool)
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-83.548996, 19.7020588, -82.1449585, 1, 0, 0, 0, 1, 0, 0, 0, 1)
 end)
 
-local armor = Section7:CreateButton("Buy High-Medium Armor", function(bool)
-    local plr = game.Players.LocalPlayer
-    local savedarmourpos = plr.Character.HumanoidRootPart.Position
-    plr.Character.HumanoidRootPart.CFrame = CFrame.new(-938.476685, -25.2498264, 570.100159, -0.0353576206, 9.85617206e-08, -0.999374807, -2.69198441e-09, 1, 9.871858e-08, 0.999374807, 6.18077589e-09, -0.0353576206)
-    wait(.2)
- 
-    fireclickdetector(game.Workspace.Ignored.Shop['[High-Medium Armor] - $2300'].ClickDetector)
-    plr.Character.HumanoidRootPart.CFrame = CFrame.new(savedarmourpos)
+local armor = Section7:CreateButton("High Medium Armor", function(bool)
+	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-939, -25, 571)
+end)
+
+local bat = Section7:CreateButton("Bat", function(bool)
+	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(380, 49, -283)
+end)
+
+local mediumarmor = Section7:CreateButton("Medium Armor", function(bool)
+	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(528, 50, -637)
 end)
 -----// PLACES -----
 local church = Section8:CreateButton("Church", function(bool)
@@ -265,10 +301,13 @@ local rejoin = Section9:CreateButton("Rejoin Server", function()
 end)
 rejoin:AddToolTip("Rejoins Server.")
 
+local hop = Section9:CreateButton("Server hop", function()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/eksotopro/holders/main/serverhop.lua'))()
+end)
 local trash = Section9:CreateButton("Trash Talk", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/eksotopro/holders/main/trash_talk.lua"))()
 end)
-trash:AddToolTip("Loads Trash Talk.")
+trash:AddToolTip("Loads Trash Talk. [J]")
 
 local macro = Section9:CreateButton("Fake Macro", function()
     loadstring(game:HttpGet("https://pastebin.com/raw/yMmUUyyE"))()
@@ -276,7 +315,7 @@ end)
 macro:AddToolTip("Loads Fake Macro. [Z]")
 
 local fly = Section9:CreateButton("Buggy Fly", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/eksotopro/holders/main/trash_talk.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/eksotopro/holders/main/fly.lua"))()
 end)
 fly:AddToolTip("Loads Buggy Fly. [X]")
 
@@ -284,6 +323,16 @@ local fullbright = Section9:CreateButton("Full Bright", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/eksotopro/holders/main/full_bright.lua"))()
 end)
 fullbright:AddToolTip("Loads Full Bright.")
+
+local nocooldown = Section9:CreateButton("No jump cooldown", function()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/eksotopro/holders/main/no-jump-cooldown.lua'))()
+end)
+nocooldown:AddToolTip("Removes jump cooldown.")
+
+local weightfarm = Section9:CreateButton("Weight Farm", function()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/eksotopro/holders/main/weight_farm.lua'))()
+end)
+weightfarm:AddToolTip("Runs Weight Farm.")
 
 local rayx = Section10:CreateButton("RAYX", function()
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/eksotopro/holders/main/cracked%20stuff/rayx.lua'))()
@@ -309,8 +358,25 @@ local crash = Section10:CreateButton("Better Crash", function()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/GS21Official/DH-TB-Crash/main/Swagmode%20crash%20modified%20(better%20FPS)%20crash'))()
 end)
 crash:AddToolTip("Loads Swag Mode Modified Crash \(For potato pc's.\)")
+
+local panic = Section10:CreateButton("Rage quit", function()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/eksotopro/holders/main/ragequit.lua'))()
+end)
+panic:AddToolTip("Why u mad bro?")
+
+local swagmode = Section10:CreateButton("Swag Mode", function()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/lerkermer/lua-projects/master/SwagModeV002'))()
+end)
+swagmode:AddToolTip("Loads Swag Mode.")
+
+local swagcrash = Section10:CreateButton("Swag Crasher", function()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/lerkermer/lua-projects/master/SuperCustomServerCrasher'))()
+end)
+swagcrash:AddToolTip("Loads Swag Mode Crash \(DOESN'T WORKS ON LOW-END PC'S\)")
 ------------------------
 ---// CREDITS PAGE \\---
+local CreditsLabel1 = Section11:CreateLabel("Made with love by hoodsense team!")
+CreditsLabel1:UpdateText("Made with love by hoodsense team!")
 local discord = Section11:CreateButton("Brought to you by hoodsense.", function(bool)
     setclipboard("ekso#0001 <@804245361894883339> | urfriend#2137 <@935540113369346130> | http://hoodsense.cf/")
     wait(1)
@@ -334,8 +400,11 @@ local discord = Section11:CreateButton("Brought to you by hoodsense.", function(
     })
 end)
 
-local partners = Section12:CreateButton("hoodsense.cf/dhc", function(bool)
-    setclipboard("http://hoodsense.cf/dhc")
+local CreditsLabel2 = Section11:CreateLabel("Our Staff")
+CreditsLabel2:UpdateText("Our Staff")
+
+local discord = Section11:CreateButton("qvit#0001", function(bool)
+    setclipboard("qvit#0001 <@804245361894883339>")
     wait(1)
     local function callback(text)
         if text == "Yes" then
@@ -349,7 +418,172 @@ local partners = Section12:CreateButton("hoodsense.cf/dhc", function(bool)
     
     game.StarterGui:SetCore("SendNotification", {
         Title = "hoodsense.cc";
-        Text = "Partners Copied on clipboard!";
+        Text = "Credits Copied on clipboard!";
+        Icon = "rbxassetid://8768441000";
+        Duration = "BLANK";
+        callbakc = bindableFunction;
+        Button1 = "Okay!";
+    })
+end)
+
+local discord = Section11:CreateButton("xdzz#2137", function(bool)
+    setclipboard("xdzz#2137 <@935540113369346130>")
+    wait(1)
+    local function callback(text)
+        if text == "Yes" then
+        print("")
+        elseif text == "No" then
+		print("")
+        end
+    end
+    
+    local bindableFunction= Instance.new("BindableFunction")
+    
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "hoodsense.cc";
+        Text = "Credits Copied on clipboard!";
+        Icon = "rbxassetid://8768441000";
+        Duration = "BLANK";
+        callbakc = bindableFunction;
+        Button1 = "Okay!";
+    })
+end)
+
+local discord = Section11:CreateButton("! ave#0005", function(bool)
+    setclipboard("! ave#0005 <@557671767565664285>")
+    wait(1)
+    local function callback(text)
+        if text == "Yes" then
+        print("")
+        elseif text == "No" then
+        print("")
+        end
+    end
+    
+    local bindableFunction= Instance.new("BindableFunction")
+    
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "hoodsense.cc";
+        Text = "Credits Copied on clipboard!";
+        Icon = "rbxassetid://8768441000";
+        Duration = "BLANK";
+        callbakc = bindableFunction;
+        Button1 = "Okay!";
+    })
+end)
+
+local discord = Section11:CreateButton("Copy website link.", function(bool)
+    setclipboard("http://hoodsense.cf")
+    wait(1)
+    local function callback(text)
+        if text == "Yes" then
+        print("")
+        elseif text == "No" then
+        print("")
+        end
+    end
+    
+    local bindableFunction= Instance.new("BindableFunction")
+    
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "hoodsense.cc";
+        Text = "Credits Copied on clipboard!";
+        Icon = "rbxassetid://8768441000";
+        Duration = "BLANK";
+        callbakc = bindableFunction;
+        Button1 = "Okay!";
+    })
+end)
+
+local partnerslabel1 = Section12:CreateLabel("Da hood cash services")
+partnerslabel1:UpdateText("Da hood cash services")
+
+local partners1 = Section12:CreateButton("discord.gg/axq", function(bool)
+    setclipboard("https://discord.gg/axq")
+    wait(1)
+    local function callback(text)
+        if text == "Yes" then
+        print("")
+        elseif text == "No" then
+        print("")
+        end
+    end
+    
+    local bindableFunction= Instance.new("BindableFunction")
+    
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "hoodsense.cc";
+        Text = "Supporter Copied on clipboard!";
+        Icon = "rbxassetid://8768441000";
+        Duration = "BLANK";
+        callbakc = bindableFunction;
+        Button1 = "Okay!";
+    })
+end)
+
+local partnerslabel2 = Section12:CreateLabel("Other supporters")
+partnerslabel2:UpdateText("Other supporters")
+
+local partners2 = Section12:CreateButton("v3rmillion.net", function(bool)
+    setclipboard("https://v3rmillion.net")
+    wait(1)
+    local function callback(text)
+        if text == "Yes" then
+        print("")
+        elseif text == "No" then
+        print("")
+        end
+    end
+    
+    local bindableFunction= Instance.new("BindableFunction")
+    
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "hoodsense.cc";
+        Text = "Supporter Copied on clipboard!";
+        Icon = "rbxassetid://8768441000";
+        Duration = "BLANK";
+        callbakc = bindableFunction;
+        Button1 = "Okay!";
+    })
+end)
+local partners = Section12:CreateButton("krnl.ca", function(bool)
+    setclipboard("https://krnl.ca")
+    wait(1)
+    local function callback(text)
+        if text == "Yes" then
+        print("")
+        elseif text == "No" then
+        print("")
+        end
+    end
+    
+    local bindableFunction= Instance.new("BindableFunction")
+    
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "hoodsense.cc";
+        Text = "Supporter Copied on clipboard!";
+        Icon = "rbxassetid://8768441000";
+        Duration = "BLANK";
+        callbakc = bindableFunction;
+        Button1 = "Okay!";
+    })
+end)
+local partners3 = Section12:CreateButton("ryos.best", function(bool)
+    setclipboard("https://ryos.best")
+    wait(1)
+    local function callback(text)
+        if text == "Yes" then
+        print("")
+        elseif text == "No" then
+        print("")
+        end
+    end
+    
+    local bindableFunction= Instance.new("BindableFunction")
+    
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "hoodsense.cc";
+        Text = "Supporter Copied on clipboard!";
         Icon = "rbxassetid://8768441000";
         Duration = "BLANK";
         callbakc = bindableFunction;
@@ -393,7 +627,7 @@ Dropdown3:SetOption("Default")
 local Colorpicker4 = Section4:CreateColorpicker("Color", function(Color)
 	Window:SetBackgroundColor(Color)
 end)
-Colorpicker4:UpdateColor(Color3.new(1,1,1))
+Colorpicker4:UpdateColor(Color3.new(91, 150, 20))
 
 local Slider3 = Section4:CreateSlider("Transparency",0,1,nil,false, function(Value)
 	Window:SetBackgroundTransparency(Value)
@@ -403,7 +637,7 @@ Slider3:SetValue(0)
 local Slider4 = Section4:CreateSlider("Tile Scale",0,1,nil,false, function(Value)
 	Window:SetTileScale(Value)
 end)
-Slider4:SetValue(0.5)
+Slider4:SetValue(1)
 
 
 -------------------------------
@@ -450,18 +684,7 @@ UserInputService.InputEnded:Connect(function(Key, Is)
         LMFAO = false
     end
 end)
- 
-game:GetService("CoreGui").imgui.Windows.Window.Visible = false
-RunService:BindToRenderStep("Aimlock", 0, function()
-    if (DaHoodSettings.AimLock and Aiming.Check() and LMFAO) then
-        local SelectedPart = Aiming.SelectedPart
- 
-        local Hit = SelectedPart.CFrame + (SelectedPart.Velocity * DaHoodSettings.Prediction)
- 
-        CurrentCamera.CFrame = CFrame.lookAt(CurrentCamera.CFrame.Position, Hit.Position)
-    end
-end)
--------------------------------
+-----------------------------------------------------------
 game.StarterGui:SetCore("SendNotification", {
     Title = "hoodsense.cc";
     Text = "Welcome to hoodsense.cc!";
@@ -470,12 +693,18 @@ game.StarterGui:SetCore("SendNotification", {
     callbakc = bindableFunction;
     Button1 = "Okay!";
 })
-
 print("_____________________HOODSENSE.CC________________________")
-print("| [hoodsense.cc] Executed version: 4.2                  |")
+print("| [hoodsense.cc] Executed version: 4.8                  |")
 print("| [hoodsense.cc] ESP is working now!                    |")
-print("| [hoodsense.cc] Join discord.gg/olifkaa                |")
 print("| [hoodsense.cc] Join us http://hoodsense.cf/discord    |")
 print("| [hoodsense.cc] Made by ekso#1337 <@804245361894883339>|")
 print("| [hoodsense.cc] Website - http://hoodsense.cf          |")
 print("_________________________________________________________")
+--[[
+██╗░░██╗░█████╗░░█████╗░██████╗░░██████╗███████╗███╗░░██╗░██████╗███████╗
+██║░░██║██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║██╔════╝██╔════╝
+███████║██║░░██║██║░░██║██║░░██║╚█████╗░█████╗░░██╔██╗██║╚█████╗░█████╗░░
+██╔══██║██║░░██║██║░░██║██║░░██║░╚═══██╗██╔══╝░░██║╚████║░╚═══██╗██╔══╝░░
+██║░░██║╚█████╔╝╚█████╔╝██████╔╝██████╔╝███████╗██║░╚███║██████╔╝███████╗
+╚═╝░░╚═╝░╚════╝░░╚════╝░╚═════╝░╚═════╝░╚══════╝╚═╝░░╚══╝╚═════╝░╚══════╝")
+]]
