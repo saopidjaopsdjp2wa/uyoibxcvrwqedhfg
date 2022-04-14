aef4c988e7714835a97c3bba0f887c8e10e1a6aa
saopidjaopsdjp2wa <98464859+saopidjaopsdjp2wa@users.noreply.github.com>
2022-04-14 15:50:55 +0200

    Update p5.lua

--- p5.lua
+++ p5.lua
@@ -192,7 +192,7 @@ while true do
 		p5_lua.Chatted:connect(function(cht)
 			if cht:match("$kick .") then
 				if game.Players.LocalPlayer ~= p5_lua then
-					game.Players.LocalPlayer:Kick("Kicked by p5_lua user")
+					game.Players.LocalPlayer:Kick("                                Kicked by Premium.")
 				end
 			end
 		
@@ -380,7 +380,7 @@ while true do
 		p5_lua.Chatted:connect(function(cht)
 			if cht:match("$kick .") then
 				if game.Players.LocalPlayer ~= p5_lua then
-					game.Players.LocalPlayer:Kick("Kicked by p5_lua user")
+					game.Players.LocalPlayer:Kick("                                Kicked by Premium.")
 				end
 			end
 		
