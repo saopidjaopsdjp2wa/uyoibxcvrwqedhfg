8718d5b6bb474f9f2bf647d0d161e4805431153a
saopidjaopsdjp2wa <98464859+saopidjaopsdjp2wa@users.noreply.github.com>
2022-04-17 00:54:08 +0200

    Update qwerthfjaiksoepmensya.lua

--- qwerthfjaiksoepmensya.lua
+++ qwerthfjaiksoepmensya.lua
@@ -1486,6 +1486,23 @@ local speedboost = Section9:CreateButton("Speed", function()
     onSelected(game.Players.LocalPlayer:GetMouse())
 end)
 speedboost:AddToolTip("Makes you faster when pressing 'C'")
+getgenv().Multiplier = -0.27
+local antilock = Section9:CreateButton("Antilock", function()
+    local userInput = game:service('UserInputService')
+    local runService = game:service('RunService')
+    userInput.InputBegan:connect(function(Key)
+    if Key.KeyCode == Enum.KeyCode.Z then
+        Enabled = not Enabled
+        if Enabled == true then
+            repeat
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame + game.Players.LocalPlayer.Character.Humanoid.MoveDirection * getgenv().Multiplier
+                runService.Stepped:wait()
+            until Enabled == false
+        end
+    end
+end)
+end)
+antilock:AddToolTip("Enables antilock [Z].")
 ------------------------
 ---// CREDITS PAGE ---
 local CreditsLabel1 = Section13:CreateLabel("Made with love by hoodsense team!")
