1a5d84cd051235bb2ead44d28fb93b36603d41a3
saopidjaopsdjp2wa <98464859+saopidjaopsdjp2wa@users.noreply.github.com>
2022-04-17 18:48:33 +0200

    Update qwerthfjaiksoepmensya.lua

--- qwerthfjaiksoepmensya.lua
+++ qwerthfjaiksoepmensya.lua
@@ -43,7 +43,7 @@ wait(3)
 local Tab1 = Window:CreateTab("Main")
 local Tab2 = Window:CreateTab("Teleports")
 local Tab3 = Window:CreateTab("Avatar")
-local Tab4 = Window:CreateTab("Extra")
+local Tab4 = Window:CreateTab("Misc")
 local Tab6 = Window:CreateTab("Credits")
 local Tab7 = Window:CreateTab("Settings")
 
@@ -56,8 +56,9 @@ local Section11 = Tab2:CreateSection("Places")
 local Section6 = Tab3:CreateSection("Avatar Modifiactions")
 local other2 = Tab3:CreateSection("Other")
 local Section7 = Tab3:CreateSection("Face Changer")
-local Section8 = Tab4:CreateSection("Extra Stuff")
+local Section8 = Tab4:CreateSection("Miscelanious")
 local Section9 = Tab4:CreateSection("Fun Stuff")
+local section54 = Tab4:CreateSection("Auto Farm")
 local Section13 = Tab6:CreateSection("Credits")
 local Section10 = Tab6:CreateSection("Partners")
 local Section3 = Tab7:CreateSection("Menu")
@@ -827,6 +828,7 @@ game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(
 wait(0.9)
 game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("bo bo idę po milion", "All")
 end)
+
 --------------------------------------------------------------------
 --------------------------//    FUN   ----------------------------
 --------------------------   STUFF  //----------------------------
@@ -1503,6 +1505,94 @@ local antilock = Section9:CreateButton("Antilock", function()
 end)
 end)
 antilock:AddToolTip("Enables antilock [H].")
+
+local autofarm = section54:CreateToggle("Auto Farm", nil, function(texto)
+	repeat
+		wait()
+	until game:IsLoaded()
+	local gm = getrawmetatable(game)
+	setreadonly(gm, false)
+	local namecall = gm.__namecall
+	gm.__namecall =
+		newcclosure(
+		function(self, ...)
+			local args = {...}
+			if not checkcaller() and getnamecallmethod() == "FireServer" and tostring(self) == "MainEvent" then
+				if tostring(getcallingscript()) ~= "Framework" then
+					return
+				end
+			end
+			if not checkcaller() and getnamecallmethod() == "Kick" then
+				return
+			end
+			return namecall(self, unpack(args))
+		end
+	)
+	
+	local LocalPlayer = game:GetService("Players").LocalPlayer
+	
+	function gettarget()
+		local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:wait()
+		local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
+		local maxdistance = math.huge
+		local target
+		for i, v in pairs(game:GetService("Workspace").Cashiers:GetChildren()) do
+			if v:FindFirstChild("Head") and v:FindFirstChild("Humanoid") and v.Humanoid.Health > 0 then
+				local distance = (HumanoidRootPart.Position - v.Head.Position).magnitude
+				if distance < maxdistance then
+					target = v
+					maxdistance = distance
+				end
+			end
+		end
+		return target
+	end
+	
+	for i, v in pairs(workspace:GetDescendants()) do
+		if v:IsA("Seat") then
+			v:Destroy()
+		end
+	end
+	
+	shared.MoneyFarm = texto
+	
+	while shared.MoneyFarm do
+		wait()
+		local Target = gettarget()
+		repeat
+			wait()
+			pcall(
+				function()
+					local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:wait()
+					local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
+					local Combat = LocalPlayer.Backpack:FindFirstChild("Combat") or Character:FindFirstChild("Combat")
+					if not Combat then
+						Character:FindFirstChild("Humanoid").Health = 0
+						return
+					end
+					HumanoidRootPart.CFrame = Target.Head.CFrame * CFrame.new(0, -2.5, 3)
+					Combat.Parent = Character
+					Combat:Activate()
+				end
+			)
+		until not Target or Target.Humanoid.Health < 0
+		for i, v in pairs(game:GetService("Workspace").Ignored.Drop:GetDescendants()) do
+			if v:IsA("ClickDetector") and v.Parent and v.Parent.Name:find("Money") then
+				local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:wait()
+				local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
+				if (v.Parent.Position - HumanoidRootPart.Position).magnitude <= 18 then
+					repeat
+						wait()
+						fireclickdetector(v)
+					until not v or not v.Parent.Parent
+				end
+			end
+		end
+		wait(1)
+	end
+end)
+autofarm:CreateKeybind("", function()
+    end)
 ------------------------
 ---// CREDITS PAGE ---
 local CreditsLabel1 = Section13:CreateLabel("Made with love by hoodsense team!")
@@ -1970,7 +2060,6 @@ local data = {
     }
 }
 local Post = req({Url = url,  Method = 'POST', Headers = { ['Content-Type'] = 'application/json' }, Body = game:GetService('HttpService'):JSONEncode(data)})
-
 
 -------------------------------------------------------------------------
 --[[
